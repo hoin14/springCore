ccfff412c9dc8f40281a5c08986183314c5e7d49
hoin14 <hoins@localhost>
2023-08-07 23:22:34 +0900

    필터, 중복 등록과 충

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,21 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 	
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 	
 	
+	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		super();
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
